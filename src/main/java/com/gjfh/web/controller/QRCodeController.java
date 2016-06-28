6179c3f843d65153927a7b7f5a6a0b8447d3a78f
Memory <liulei_1001@163.com>
2016-06-28 23:09:28 +0800

    update ORcode

--- src/main/java/com/gjfh/web/controller/QRCodeController.java
+++ src/main/java/com/gjfh/web/controller/QRCodeController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.Hashtable;
 
 /**
@@ -25,15 +28,16 @@ public class QRCodeController {
     @RequestMapping("/generateQRCode")
     @ResponseBody
     public String generateQRCode() throws WriterException, IOException {
-        String urlStr = "www.jobook.cn";
-        String localPath = "../statics/qrcode/";
+        String urlStr = "http://www.jobook.cn";
+        String localPath = "/Users/Sky/Development/test";
+        String fileName = "test.png";
         BitMatrix byteMatrix;
         try {
             Hashtable<EncodeHintType, Integer> hints = new Hashtable<EncodeHintType, Integer>();
             hints.put(EncodeHintType.MARGIN, 1);
             byteMatrix = new MultiFormatWriter().encode(new String(urlStr.getBytes("UTF-8"), "iso-8859-1"), BarcodeFormat.QR_CODE, 200, 200, hints);
-            File file = new File(localPath);
-            MatrixToImageWriter.writeToFile(byteMatrix, "png", file);
+            Path path = FileSystems.getDefault().getPath(localPath, fileName);
+            MatrixToImageWriter.writeToPath(byteMatrix, "png", path);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
